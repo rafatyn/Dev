8e83e5c913625c01df9773d20032f1757519be30
rafatyn <rafatyn@hotmail.com>
2020-07-29 14:21:01 +0200

    gitignored Udpated

--- Sparky-core (C++ y OpenGL)/Sparky-core (Develop)/src/SparkyGl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef SPARKY_EMSCRIPTEN
-	#define GLFW_INCLUDE_ES3
-#else
-	#include <GL\glew.h>
-#endif
-
-#include <GLFW/glfw3.h>
--- Sparky-core (C++ y OpenGL)/Sparky-core (Develop)/src/maths/maths.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <math.h>
-#include "Maths_func.h"
-#include "Vec2f.h"
-#include "Vec3f.h"
-#include "Vec4f.h"
-#include "Mat4.h"
